21fd026d63d28abaf4adb7fb351b5212db12db83
Patrick McCarthy <p.t.mccarthy@gmail.com>
2016-01-12 13:50:06 -0800

    Add user profile page

--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm
+from datetime import datetime
+from .forms import LoginForm, EditForm
 from .models import User
 
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 @lm.user_loader
 def load_user(id):
@@ -42,6 +47,36 @@ def login():
 
     return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
+
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == '':
@@ -65,4 +100,4 @@ def after_login(resp):
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))    
+    return redirect(url_for('index'))
