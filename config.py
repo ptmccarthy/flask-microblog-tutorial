b025501a253f82cf4afc15f9e4856be81a858d7a
Patrick McCarthy <p.t.mccarthy@gmail.com>
2016-01-11 18:32:58 -0800

    Finish login form and routes

---
+++ config.py
@@ -0,0 +1,9 @@
+WTF_CSRF_ENABLED = True
+# bad form here, but just a throwaway tutorial so whatever
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
